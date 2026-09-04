784f22c54b66d0d17417610f1fc62ea548cb6326
Xavier <xtanjw@gmail.com>
2026-09-04 15:58:57 +0800

    Update filerenamer.py
    Added .

--- filerenamer.py
+++ filerenamer.py
@@ -14,14 +14,14 @@ dynnum = args.dynnum
 verbose = args.verbose
 basename = name[0:name.find('#')] + str(dynnum) + name[name.find('#') + 1:len(name)]
 if p.is_absolute():
-    for files in p.glob('*' + extension):
+    for files in p.glob('*.' + extension):
         renamed = name[0:name.find('#')] + str(dynnum) + name[name.find('#')+1:len(name)] + '.' + extension
         files.rename(p / renamed)
         dynnum += 1
         if verbose:
             print(f'File "{files}" has been renamed to "{p / renamed}"')
 else:
-    for files in p.glob('*' + extension):
+    for files in p.glob('*.' + extension):
         renamed = name[0:name.find('#')] + str(dynnum) + name[name.find('#') + 1:len(name)] + '.' + extension
         files.rename(p / renamed)
         dynnum += 1
